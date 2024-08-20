1f081cf3a9cb7a6f47e49adeaf543abe6506d6bc
baptiste-veyrard <48984863+baptiste-veyrard@users.noreply.github.com>
2024-08-20 23:53:38 +0200

    first push

--- PowerSyncExample/_Secrets.swift
+++ PowerSyncExample/_Secrets.swift
@@ -2,7 +2,7 @@ import Foundation
 
 // Enter your Supabase and PowerSync project details.
 enum Secrets {
-    static let powerSyncEndpoint = "https://foo.powersync.journeyapps.com"
-    static let supabaseURL = URL(string: "https://foo.supabase.co")!
-    static let supabaseAnonKey = "foo"
+    static let powerSyncEndpoint = "https://66c4f7e1619fa3cbfad4ef99.powersync.journeyapps.com"
+    static let supabaseURL = URL(string: "https://gqfmadcpixiqjdaylrln.supabase.co")!
+    static let supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdxZm1hZGNwaXhpcWpkYXlscmxuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQxODQzMzcsImV4cCI6MjAzOTc2MDMzN30.Vaefl_PlJ6SH8MQfC22XJcjLCyGeWHGCj6deP_zw-QM"
 }
